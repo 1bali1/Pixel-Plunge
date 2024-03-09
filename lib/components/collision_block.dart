140f13188461b90fe908f042ed9a249cb0cf250c
Bali <138283967+1bali1@users.noreply.github.com>
2024-03-09 16:17:53 +0100

    Delete lib directory

--- lib/components/collision_block.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flame/components.dart';
-
-class CollisionBlock extends PositionComponent{
-  bool isPlatform;
-  bool isBorderA;
-  bool isBorderB;
-  CollisionBlock({position, size, this.isPlatform = false, this.isBorderA = false, this.isBorderB = false}) : super(position: position, size: size){debugMode = false;}
-
-}
